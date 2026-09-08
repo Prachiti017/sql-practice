a05acea9298a0ef6677d1dd71c385000bcfd52a0
Prachiti Hole <114359925+Prachiti017@users.noreply.github.com>
2026-09-08 19:35:18 +0530

    SQL JOIN examples
    Added examples of INNER JOIN, LEFT JOIN, and RIGHT JOIN in SQL.

---
+++ 02-joins/Joins.sql
@@ -0,0 +1,29 @@
+-- =========================================
+-- SQL JOINS
+-- =========================================
+
+-- INNER JOIN
+SELECT
+    e.name,
+    d.department_name
+FROM employees e
+INNER JOIN departments d
+ON e.department_id = d.department_id;
+
+
+-- LEFT JOIN
+SELECT
+    e.name,
+    d.department_name
+FROM employees e
+LEFT JOIN departments d
+ON e.department_id = d.department_id;
+
+
+-- RIGHT JOIN
+SELECT
+    e.name,
+    d.department_name
+FROM employees e
+RIGHT JOIN departments d
+ON e.department_id = d.department_id;
